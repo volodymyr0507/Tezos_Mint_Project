76ed63eec123f7468b19838ae11b8349c4ad1f20
talentedev <webdev124@outlook.com>
2021-07-15 12:10:28 +0300

    camera and photo gallery using react and ionic

---
+++ src/pages/Home.tsx
@@ -0,0 +1,51 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg, IonFab, IonFabButton, IonIcon } from '@ionic/react';
+import React, { Component } from 'react';
+import { Camera, CameraResultType } from '@capacitor/camera';
+// import { defineCustomElements } from '@ionic/pwa-elements/loader';
+const INITIAL_STATE = {
+  photo: '',
+};
+export class Home extends Component {
+  state: any = {};
+  props: any = {};
+  constructor(props: any) {
+    super(props);
+    this.state = { ...INITIAL_STATE };
+    // defineCustomElements(window);
+  }
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri
+    });
+    var imageUrl = image.webPath;
+
+    // Can be set to the src of an image now
+    this.setState({
+      photo: imageUrl
+    })
+  }
+
+  render() {
+    const { photo } = this.state;
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Ionic Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <IonImg style={{ 'border': '1px solid black', 'minHeight': '100px' }} src={photo} ></IonImg>
+          <IonFab color="primary" vertical="bottom" horizontal="center" slot="fixed">
+            <IonFabButton color="primary" onClick={() => this.takePicture()}>
+              <IonIcon name="add" />
+            </IonFabButton>
+          </IonFab>
+        </IonContent>
+      </IonPage >
+    );
+  };
+}
+export default Home;
